2c5c0719667ec1a14d7ec5749943fb5a134691a2
Jesse Varnado <jesse.varnado@gmail.com>
2016-01-04 15:54:32 -0600

    update with example from chotolatey.org

--- install_frc_tools.bat
+++ install_frc_tools.bat
@@ -1,5 +1,5 @@
 # Install Chocolatey
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 # Install FRC tools 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/nolabots/frc_tools/master/frc_tools.ps1'))" 
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/nolabots/frc_tools/master/frc_tools.ps1'))" 
