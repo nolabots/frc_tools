d614f2c6302b39cfffaad6e706f155872b3b53d5
Jesse Varnado <jesse.varnado@gmail.com>
2016-01-09 00:48:37 -0600

    accept licenses

--- frc_tools.ps1
+++ frc_tools.ps1
@@ -1,3 +1,4 @@
 # FRC Tools
-cinst github
-cinst git -s "https://raw.github.com/nolabots/frc_tools/master/eclipse.4.5.1.nupkg"
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+cinst github -y
+cinst git -s "https://raw.github.com/nolabots/frc_tools/master/eclipse.4.5.1.nupkg" -y
