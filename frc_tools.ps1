6258954f172e61e188ada403cab229aae1bd01ca
Jesse Varnado <jesse.varnado@gmail.com>
2016-01-09 01:05:23 -0600

    install eclipse from package

--- frc_tools.ps1
+++ frc_tools.ps1
@@ -1,4 +1,4 @@
 # FRC Tools
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 cinst github -y
-cinst git -s "https://raw.github.com/nolabots/frc_tools/master/eclipse.4.5.1.nupkg" -y
+cinst eclipse.4.5.1.nupkg -s "https://raw.github.com/nolabots/frc_tools/master/" -y
