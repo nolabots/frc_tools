5cf9a6475c3d1f73e27e8e4324548be5102eb3e6
Jesse Varnado <jesse.varnado@gmail.com>
2016-01-12 14:54:39 -0600

    fix eclipse installation
    download the eclipse nupkg file then execute it

--- frc_tools.ps1
+++ frc_tools.ps1
@@ -1,4 +1,5 @@
 # FRC Tools
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-cinst chrome slack github -y
-cinst eclipse.4.5.1.nupkg -s "https://raw.github.com/nolabots/frc_tools/master/" -y
+cinst slack github jdk8 -y
+(New-Object System.Net.WebClient).DownloadFile("https://raw.github.com/nolabots/frc_tools/master/eclipse.4.5.1.nupkg", "$env:TEMP\eclipse.4.5.1.nupkg")
+cinst "$env:TEMP\eclipse.4.5.1.nupkg" -y
